15b91d2af601b858b961f1e1a10493166183c8a8
Alshaiban1 <139134530+Alshaiban1@users.noreply.github.com>
2023-08-23 23:07:44 +0300

    Add files via upload

---
+++ transfer.php
@@ -0,0 +1,23 @@
+<?php 
+//Sending direction value to database
+
+      
+include ("database.php");  // Including the database.php
+
+
+if (isset($_POST)) //if statment which doesn't excute code if $_POST is is not set
+
+{
+    
+$variable = implode($_POST); //Implode $_POST from array to string and save it in $variable
+
+  
+$sql ="INSERT INTO directionvalues (value) VALUES ('$variable')"; //Send $variable string to the database, and saving it in the directionvalues table, inside 'value' coloumn
+
+mysqli_query ($connection, $sql); 
+
+
+}
+mysqli_close($connection);
+?> 
+
